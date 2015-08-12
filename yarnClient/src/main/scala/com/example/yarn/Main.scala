7764e69ff331d7113ec0cea3fe429698a5984715
shibata_ko <shibata_ko@worksap.co.jp>
2015-08-12 16:56:10 +0900

    spark

--- yarnClient/src/main/scala/com/example/yarn/Main.scala
+++ yarnClient/src/main/scala/com/example/yarn/Main.scala
@@ -3,7 +3,7 @@ package com.example.yarn
 object Main {
 
 	val sparkAppPath = "hdfs://192.168.133.214/jarFiles/3_1_edp2-spark-batch-sample-2.0.0-SNAPSHOT.jar"
-	val entryPoint = "com.worksap.company.edp2.sample.spark.batch.SparkSample"
+	val entryPoint = "com.example.SparkSample"
 	val uniqueName = "uniqueName"
 
 	def main(args: Array[String]) {
