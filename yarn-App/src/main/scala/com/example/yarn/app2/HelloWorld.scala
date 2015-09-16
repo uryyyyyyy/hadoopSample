3fa1577761b533806957f823c74b5da88613bc8c
uryyyyyyy <koki305@gmail.com>
2015-09-16 23:23:15 +0900

    add yarn app

---
+++ yarn-App/src/main/scala/com/example/yarn/app2/HelloWorld.scala
@@ -0,0 +1,11 @@
+package com.example.yarn.app2
+
+/**
+ * Hello world program which runs in a container
+ */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("helloworld")
+  }
+
+}
