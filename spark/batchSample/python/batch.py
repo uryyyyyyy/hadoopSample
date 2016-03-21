57ae5ce92f205ed6ca21bd062bb9cddb9cfbed7f
uryyyyyyy <koki305@gmail.com>
2016-03-22 01:07:32 +0900

    python to s3

--- spark/batchSample/python/batch.py
+++ spark/batchSample/python/batch.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     rdd = sc.range(1, 1000)
 
     counts = rdd.map(lambda i: i * 2)
-    counts.saveAsTextFile("/tmp/pythooon")
+    counts.saveAsTextFile("s3://uryyyyyyy-sandbox/py.log")
