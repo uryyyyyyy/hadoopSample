6b1a77b75b6345b1616a4ae4e7e8e823675ff870
uryyyyyyy <koki305@gmail.com>
2016-05-30 22:50:35 +0900

    test

--- spark/scala_2.11/batch_helloWorld/build.sbt
+++ spark/scala_2.11/batch_helloWorld/build.sbt
@@ -4,7 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
+lazy val sparkVersion = "1.6.1"
+
 libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion % "test"
 )
 
 assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
--- spark/scala_2.11/batch_helloWorld/src/test/scala/HelloSpec.scala
+++ spark/scala_2.11/batch_helloWorld/src/test/scala/HelloSpec.scala
@@ -1,7 +1,18 @@
-//import org.scalatest._
-//
-//class HelloSpec extends FlatSpec with Matchers {
-//  "Hello" should "have tests" in {
-//    true should === (true)
-//  }
-//}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest._
+
+class HelloSpec extends FunSpec with MustMatchers {
+
+  describe("SparkRDD") {
+    it("test1") {
+      val conf = new SparkConf().setAppName("Simple Application").setMaster("local[3]")
+      val sc = new SparkContext(conf)
+
+      val rdd = sc.parallelize(1 to 10)
+      val value = rdd.map(_ * 2).reduce(_ + _)
+      value mustBe 110
+
+      sc.stop()
+    }
+  }
+}
