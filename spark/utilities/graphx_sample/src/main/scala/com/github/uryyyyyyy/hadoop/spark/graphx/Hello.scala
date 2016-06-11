f2bf5243032e41e0c60fcd1c57c13483749b9150
uryyyyyyy <koki305@gmail.com>
2016-06-11 14:11:16 +0900

    mllib

--- spark/utilities/graphx_sample/src/main/scala/com/github/uryyyyyyy/hadoop/spark/graphx/Hello.scala
+++ spark/utilities/graphx_sample/src/main/scala/com/github/uryyyyyyy/hadoop/spark/graphx/Hello.scala
@@ -9,13 +9,25 @@ object Hello {
 
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
+
+    val vertexes = Array(
+      (3L, ("rxin", "student")),
+      (7L, ("jgonzal", "postdoc")),
+      (5L, ("franklin", "prof")),
+      (2L, ("istoica", "prof"))
+    )
     val users: RDD[(VertexId, (String, String))] =
-      sc.parallelize(Array((3L, ("rxin", "student")), (7L, ("jgonzal", "postdoc")),
-        (5L, ("franklin", "prof")), (2L, ("istoica", "prof"))))
+      sc.parallelize(vertexes)
+
+    val edges = Array(
+      Edge(3L, 7L, "collab"),
+      Edge(5L, 3L, "advisor"),
+      Edge(2L, 5L, "colleague"),
+      Edge(5L, 7L, "pi")
+    )
     // Create an RDD for edges
     val relationships: RDD[Edge[String]] =
-      sc.parallelize(Array(Edge(3L, 7L, "collab"),    Edge(5L, 3L, "advisor"),
-        Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
+      sc.parallelize(edges)
     // Define a default user in case there are relationship with missing user
     val defaultUser = ("John Doe", "Missing")
     // Build the initial Graph
